6d84415b5c2f89e70665f3127eaac6733e38fde6
webgently <oleksandrshkabura811@gmail.com>
2023-08-18 00:40:19 +0300

    feat: integration register api integration

--- resources/js/services/auth.service.js
+++ resources/js/services/auth.service.js
@@ -2,6 +2,18 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8000/api/auth/';
 
-class AuthService {}
+class AuthService {
+  register(user) {
+    return axios
+      .post(API_URL + 'register', {
+        name: user.name,
+        email: user.email,
+        password: user.password
+      })
+      .then((response) => {
+        return response;
+      });
+  }
+}
 
 export default new AuthService();
--- resources/js/store/auth.module.js
+++ resources/js/store/auth.module.js
@@ -1,3 +1,31 @@
 import AuthService from '../services/auth.service';
 
-export const auth = {};
+const user = JSON.parse(localStorage.getItem('user'));
+const initialState = user ? { status: { loggedIn: true }, user } : { status: { loggedIn: false }, user: null };
+
+export const auth = {
+  namespaced: true,
+  state: initialState,
+  actions: {
+    register({ commit }, user) {
+      return AuthService.register(user).then(
+        (response) => {
+          commit('registerSuccess');
+          return Promise.resolve(response);
+        },
+        (error) => {
+          commit('registerFailure');
+          return Promise.reject(error);
+        }
+      );
+    }
+  },
+  mutations: {
+    registerSuccess(state) {
+      state.status.loggedIn = false;
+    },
+    registerFailure(state) {
+      state.status.loggedIn = false;
+    }
+  }
+};
