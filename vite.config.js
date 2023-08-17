2e68f72b164d6fb607bcf5fc63e366a67a3e9175
webgently <oleksandrshkabura811@gmail.com>
2023-08-17 23:21:02 +0300

    dep: installing prettier module and settting prettier config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    plugins: [vue(), laravel(["resources/css/app.css", "resources/js/app.js"])],
+});
